09620c38ab800d0579c0970cd42b752d7d735914
Abduqaxxorov Ismoil <isroilovich004@gmail.com>
2023-06-10 16:50:53 +0500

    salom

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 'use strict';
 
 
-const box = document.querySelector('.box')
-setTimeout( () => {
-    box.style.backgroundColor = "red"
-}, 2000 );
+// const box = document.querySelector('.box')
+// // setTimeout( () => {
+// //     box.style.backgroundColor = "red"
+// // }, 2000 );
 
-let count = 0;
-const countInterval = setInterval( count(), 1000);
+
+
+// let counter = 0;
+// const countInterval = setInterval( count, 1000);
+
+
+
+// function count() {
+//     console.log(counter++);
+//     if(counter > 5 ) {
+//         clearInterval(countInterval)
+//     }
+//     if(counter > 5 ) {
+//         box.style.marginLeft = "auto";
+//     }
+// }
+const box = document.querySelector('.box')
+const body = document.querySelector('body')
+
+let counter = 0;
+const countInterval = setInterval( count, 1000);
+
 
 function count() {
-    console.log(count++);
-    if( count > 5 ) {
-        clearInterval()
+    console.log(counter++)
+    if(counter > 5 ) {
+        clearInterval(countInterval)
+    }
+    if(counter > 5 ) {
+        box.style.display = "block"
+        body.style.backgroundColor = '#3e3f4e'
     }
 }
+
+const span = document.querySelector('span');
+
+span.addEventListener('click', () => {
+    box.style.display = "none";
+    body.style.display = "none";
+})
+
+
+
+
+
+
