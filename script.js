bcbadbf72b90fe762cbf4909c316f754c581f26c
Abduqaxxorov Ismoil <isroilovich004@gmail.com>
2023-06-10 15:53:56 +0500

    create script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const box = document.querySelector('.box')
 setTimeout( () => {
     box.style.backgroundColor = "red"
 }, 2000 );
+
+let count = 0;
+const countInterval = setInterval( count(), 1000);
+
+function count() {
+    console.log(count++);
+    if( count > 5 ) {
+        clearInterval()
+    }
+}
